f159ee0c20cb63077a5cd59394629e0fa5c435c4
darkseid <gh0stv3g3ta@gmail.com>
2024-05-03 07:45:14 +0530

    First

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![allow(dead_code)]
 fn main() {
     println!("Hello, world!");
+}
+
+// function to add two numbers
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
